61f3503fb048469bb0a739b34306debf3d929303
Vinícius Guilherme Palu <vinicius.palu@live.com>
2017-03-14 22:29:20 -0300

    Exercises 01, 02, 03 and 04 are complete. Trying to do number 05.

---
+++ src/Exercicio_05.java
@@ -0,0 +1,55 @@
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by Palups on 14/03/2017.
+ */
+public class Exercicio_05 {
+
+    public Color getRGBf(){
+        return getRGBf();
+    }
+
+    public Color getRGBAf(){
+        return getRGBAf();
+    }
+
+    public Color setRGBf(){
+
+    }
+
+    public Color setRGBAf(){
+
+    }
+
+    BufferedImage multiply(BufferedImage src, float[] color){
+        BufferedImage out = new BufferedImage(src.getWidth(), src.getHeight(), BufferedImage.TYPE_INT_RGB);
+
+        for(int y = 0; y < src.getHeight(); y++){
+            for(int x = 0; x < src.getWidth(); x++){
+                Color srcColor = new Color(src.getRGB(x, y));
+                int r = srcColor.getRed();
+                int g = srcColor.getGreen();
+                int b = srcColor.getBlue();
+
+            }
+        }
+        return out;
+    }
+
+    public void run() throws IOException {
+        String PATH = "C:\\Users\\Palups\\Documents\\img\\cor";
+        BufferedImage img = ImageIO.read(new File(PATH, "puppy.png"));
+
+        BufferedImage result_img = multiply(img, 0.5f);
+
+        ImageIO.write(result_img, "png", new File("result_puppy.png"));
+    }
+
+    public static void main(String [] args) throws IOException {
+        new Exercicio_05().run();
+    }
+}
